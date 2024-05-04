f458cd2d1d77dc348140f46fa4742603e2d9be52
coax-hectic <168881123+coax-hectic@users.noreply.github.com>
2024-05-04 06:34:20 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:coax-hectic/vue-pages.git main:gh-pages
+git push -f git@github.com:coax-hectic/coax-hectic.github.io.git main:gh-pages
 cd -
